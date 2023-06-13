73fd8132ee4187d43e400fe1b5ce1a3713730afc
kodanne COULIBALY <kodanne.bakeli@gmail.com>
2023-06-13 09:03:30 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// let age = prompt("Saisir votre age");
+
+//  if(age <= 14){
+//      alert("Vous etes un Enfant");
+//  } else if (age <= 25){
+//      alert("Vous etes un Adolescent");
+//  } else if(age <=64){
+//      alert("Vous etes un Adulte");
+//  } else if(age >=65){
+//     alert("Vous etes un Aîné");
+// }else {
+//    alert ("veuillez entrer un nombre")
+// }
+
+var age =  prompt("Saisir votre age");
+
+    age <= 14 ? alert("Vous etes un enfant")
+         : age <= 25 ? alert("Vous etes un Adolescent")
+         : age <= 64 ? alert("Vous etes un Adulte")
+         : age >= 65 ? alert("Vous etes un Aîné")
+         :alert( "veuillez entrer un nombre");
+         
+  
+ 
